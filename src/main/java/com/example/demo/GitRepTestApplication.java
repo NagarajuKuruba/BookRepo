672563a5f5be570e0355133ce0bbcb75f505c00f
Amma <Amma@192.168.10.8>
2018-06-26 22:55:17 +0530

    commit

---
+++ src/main/java/com/example/demo/GitRepTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepTestApplication.class, args);
+	}
+}
